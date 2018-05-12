5f00ef01ecad8e146a7448768387d72922b36d66
ishraqe <kanak431@diu.edu.bd>
2018-05-13 01:04:50 +0600

    splash added

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import { AppRegistry } from "react-native";
 import App from "./src/App";
 
 AppRegistry.registerComponent("chuti", () => App);
-=======
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('chuti', () => App);
->>>>>>> d4df611a4fd5b6f53808b1b7527cdee4eab2f8dc
