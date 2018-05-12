3d57c827bfe5ccaf1dbff41de4b6013912ed2e5e
ishraqe <kanak431@diu.edu.bd>
2018-05-13 01:21:13 +0600

    splash added final

--- android/app/src/main/java/com/chuti/SplashActivity.java
+++ android/app/src/main/java/com/chuti/SplashActivity.java
@@ -2,6 +2,7 @@ package com.chuti;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,13 +10,23 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SplashActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
+        new Handler().postDelayed(new Runnable() {
+ 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            }, SPLASH_TIME_OUT);
+        }
 }
